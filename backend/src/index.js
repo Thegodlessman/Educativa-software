10642ec0070faacc05b4562385f6ad6e8656ae36
Thegodlessman <luischang16@hotmail.com>
2025-06-16 00:47:48 -0400

    FEAT: Algo de cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,7 +16,9 @@ const app = express()
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
-app.use(cors());
+app.use(cors({
+    origin: process.env.FRONTEND_URL
+}));
 
 app.use(userRoutes)
 app.use(profileRoutes)
